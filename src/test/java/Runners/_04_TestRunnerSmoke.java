b156aa056845c767ba3908866137facd8ded56f8
akaymakci <akaymakci@gmail.com>
2023-04-07 03:09:05 +0200

    cucumber report plugin chnage back to previous state.2

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
-        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}  // master report için sonradan ekleniyor
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json" }  // master report için sonradan ekleniyor
         //plugin={"html:target//cucumber-reports.html"}
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
